c820e7738b5cd2a6a8a8fb5936262369dcda78ec
Zeyd Asadullayev <asa.zeyd@gmail.com>
2023-12-19 17:54:16 +0400

    required

--- js/script.js
+++ js/script.js
@@ -18,6 +18,27 @@ $(document).ready(() => {
     topSpacing: 75,
   });
 });
+// subscribe
+const subscribeEmail = document.getElementById("subscribe-email");
+
+const sendSubmit = (e) => {
+  e.preventDefault();
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "asa.zeyd@gmail.com",
+    Password: "23FCFA4CF198B825D889A86E2A496B11543B",
+    To: "asa.zeyd@gmail.com",
+    From: "asa.zeyd@gmail.com",
+    Subject: "Your Subscribe from Tortelliani Rest",
+    Body: `
+    Email: ${contactEmail.value}
+    `,
+  }).then((message) => {
+    alert(message);
+  });
+  subscribeEmail.value = "";
+};
+
 // Contact us
 let contactName = document.getElementById("contact-name");
 let contactEmail = document.getElementById("contact-email");
@@ -31,7 +52,18 @@ const sendContactForm = (e) => {
     Password: "23FCFA4CF198B825D889A86E2A496B11543B",
     To: "asa.zeyd@gmail.com",
     From: "asa.zeyd@gmail.com",
-    Subject: "This is the subject",
-    Body: "",
-  }).then((message) => alert(message));
+    Subject: "Request from Restraunt",
+    Body: `
+    Name: ${contactName.value}
+    <br>
+    Email: ${contactEmail.value}
+    <br>
+    Message: ${contactMessage.value}
+    `,
+  }).then((message) => {
+    alert(message);
+    contactName.value = "";
+    contactEmail.value = "";
+    contactMessage.value = "";
+  });
 };
